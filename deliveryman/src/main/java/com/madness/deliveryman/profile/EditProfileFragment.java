bcaa29aab31bf97c71b6c4c2be0e83f85ceec86a
NicOrganizer <nicolafioranelli@gmail.com>
2019-05-17 12:43:31 +0200

    test

--- deliveryman/src/main/java/com/madness/deliveryman/profile/EditProfileFragment.java
+++ deliveryman/src/main/java/com/madness/deliveryman/profile/EditProfileFragment.java
@@ -43,6 +43,8 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
+// TODO:    modify
+
 public class EditProfileFragment extends Fragment {
 
     private EditText fullname;
