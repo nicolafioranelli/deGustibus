9a5a07f3a377dd69d29dab813d036164ecf3e349
Nicola <nicola.sabino94@gmail.com>
2019-06-05 17:13:40 +0200

    fix on new daily offer picture

--- restaurant/src/main/java/com/madness/restaurant/daily/NewDailyOffer.java
+++ restaurant/src/main/java/com/madness/restaurant/daily/NewDailyOffer.java
@@ -172,31 +172,25 @@ public class NewDailyOffer extends Fragment {
     }
 
     public void getPhoto(View v) {
-        ImageView imageView = getActivity().findViewById(R.id.imageviewfordish);
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder pictureDialog = new AlertDialog.Builder(getActivity());
-                pictureDialog.setTitle(getActivity().getString(R.string.select_action));
-                String[] pictureDialogItems = {
-                        getResources().getText(R.string.camera).toString(), getResources().getText(R.string.gallery).toString()};
-                pictureDialog.setItems(pictureDialogItems,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                switch (which) {
-                                    case 0:
-                                        checkPermissionsAndStartCamera();
-                                        break;
-                                    case 1:
-                                        checkPermissionsAndStartGallery();
-                                        break;
-                                }
-                            }
-                        });
-                pictureDialog.show();
-            }
-        });
+        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(getActivity());
+        pictureDialog.setTitle(getActivity().getString(R.string.select_action));
+        String[] pictureDialogItems = {
+                getResources().getText(R.string.camera).toString(), getResources().getText(R.string.gallery).toString()};
+        pictureDialog.setItems(pictureDialogItems,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                checkPermissionsAndStartCamera();
+                                break;
+                            case 1:
+                                checkPermissionsAndStartGallery();
+                                break;
+                        }
+                    }
+                });
+        pictureDialog.show();
     }
 
     private String getPrefPhoto() {
