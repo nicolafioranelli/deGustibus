5906e63b2c9064d1596f5a4adb190a4503391ab2
Nicola <nicola.sabino94@gmail.com>
2019-06-06 22:10:29 +0200

    few changes

--- restaurant/src/main/java/com/madness/restaurant/reservations/RiderChoiceFragment.java
+++ restaurant/src/main/java/com/madness/restaurant/reservations/RiderChoiceFragment.java
@@ -15,6 +15,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.firebase.geofire.GeoFire;
@@ -167,6 +170,12 @@ public class RiderChoiceFragment extends Fragment {
         final TextView price = view.findViewById(R.id.price);
         final Button button = view.findViewById(R.id.orderButton);
         final Button refuse = view.findViewById(R.id.refuseButton);
+
+        // review items
+        final ImageView riderImage = view.findViewById(R.id.delImage);
+        final TextView riderName = view.findViewById(R.id.deliverymanName);
+        final RatingBar rating = view.findViewById(R.id.ratingBar);
+        final EditText comment = view.findViewById(R.id.comment);
 
         summaryReference = databaseReference.child("orders").child(id);
         summaryListener = summaryReference.addValueEventListener(new ValueEventListener() {
