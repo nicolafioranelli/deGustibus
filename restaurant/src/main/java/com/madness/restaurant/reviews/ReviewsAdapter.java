9bd8a2e16ede3fdafb3eaec546f699773df3ff20
Nicola <nicola.sabino94@gmail.com>
2019-06-06 09:52:51 +0200

    new files on restaurant

---
+++ restaurant/src/main/java/com/madness/restaurant/reviews/ReviewsAdapter.java
@@ -0,0 +1,95 @@
+package com.madness.restaurant.reviews;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.madness.deliveryman.R;
+
+import java.util.List;
+
+public class ReviewsAdapter extends RecyclerView.Adapter<ReviewsAdapter.ReviewsHolder> {
+
+    Context context;
+    View view;
+    List<ReviewsComparable> riderList;
+
+    public ReviewsAdapter(Context context, View view, List<ReviewsComparable> riderList) {
+        this.context = context;
+        this.view = view;
+        this.riderList = riderList;
+    }
+
+    /**
+     * Generate a single listitem
+     * @param viewGroup
+     * @param i
+     * @return
+     */
+    @NonNull
+    @Override
+    public ReviewsHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.rider_reviews_listitem, viewGroup, false);
+        ReviewsHolder viewHolder = new ReviewsHolder(view);
+        return viewHolder;
+    }
+
+    /**
+     * Populate the listitem
+     * @param holder reviewholder
+     * @param position in the set
+     */
+    @Override
+    public void onBindViewHolder(@NonNull ReviewsHolder holder, int position) {
+        final ReviewsComparable riderReviewsComparable = riderList.get(position);
+        holder.name.setText(riderReviewsComparable.getName());
+        holder.comment.setText(riderReviewsComparable.getComment());
+        holder.date.setText(riderReviewsComparable.getDate());
+        holder.restSimpleRatingBar.setNumStars(5);
+        holder.restSimpleRatingBar.setRating(Float.valueOf(riderReviewsComparable.getRating()));
+    }
+
+    /**
+     * Get the size of the set
+     * @return
+     */
+    @Override
+    public int getItemCount() {
+        return riderList.size();
+    }
+
+    /**
+     * Update the set of reviews
+     * @param viewModels
+     */
+    public void updateData(List<ReviewsComparable> viewModels) {
+        for (int i = 0; i < viewModels.size(); i++) {
+            riderList.set(i, viewModels.get(i));
+        }
+        notifyDataSetChanged();
+    }
+
+
+    /**
+     * ViewHolder class
+     */
+    class ReviewsHolder extends RecyclerView.ViewHolder {
+
+        public TextView name, comment, date;
+        RatingBar restSimpleRatingBar;
+
+        public ReviewsHolder(final View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.name);
+            date = itemView.findViewById(R.id.date);
+            restSimpleRatingBar = itemView.findViewById(R.id.restSimpleRatingBar);
+            comment = itemView.findViewById(R.id.rider_comment);
+        }
+    }
+}
+
---
+++ restaurant/src/main/java/com/madness/restaurant/reviews/ReviewsComparable.java
@@ -0,0 +1,62 @@
+package com.madness.restaurant.reviews;
+
+public class ReviewsComparable {
+
+    private String date;
+    private String rating;
+    private String name;
+    private String comment;
+    private String key;
+
+
+    public ReviewsComparable(String date, String rating, String name, String comment, String key) {
+        this.date = date;
+        this.rating = rating;
+        this.name = name;
+        this.comment = comment;
+        this.key = key;
+    }
+
+    public ReviewsComparable() {
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getRating() {
+        return rating;}
+
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
